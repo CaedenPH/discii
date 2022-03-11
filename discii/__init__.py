4da7c830e57898dda64a7f6fc5455acdc959a65d
Caeden <caedenperelliharris@gmail.com>
2022-03-11 14:50:37 +0000

    Update __init__.py

--- discii/__init__.py
+++ discii/__init__.py
@@ -3,5 +3,5 @@ __version__ = "0.0.1"
 __author__ = "CaedenPH"
 __license__ = "MIT"
 
-from .client import Client  # noqa: ignore
-from .errors import InvalidBotToken  # noqa: ignore
+from .client import Client  # noqa: 401
+from .errors import InvalidBotToken  # noqa: 401
