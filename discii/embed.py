6399cfca6452f7aa9fd7dbd99623909d4e17f79b
Caeden <caedenperelliharris@gmail.com>
2022-03-14 21:44:07 +0000

    add embed documentation

--- discii/embed.py
+++ discii/embed.py
@@ -19,6 +19,26 @@ class Embed:
     ----------
     type: :class:`str`
         The embed type.
+    title: :class:`str`
+        The embed title.
+    description: :class:`str`
+        The embed description.
+    colour: :class:`int`
+        The embed colour in hex format.
+    timestamp: :class:`datetime`
+        The ISO8601 timestamp.
+    thumbnail: :class:`Optional[Dict[str, str]]`
+        The embed thumbnail.
+    video: :class:`Optional[Dict[str, str]]`
+        The embed video.
+    image: :class:`Optional[Dict[str, str]]`
+        The embed image.
+    author: :class:`Optional[Dict[str, Optional[str]]]`
+        The embed author.
+    footer: :class:`Optional[Dict[str, Optional[str]]]`
+        The embed footer.
+    fields: :class:`List[Dict[str, Union[str, bool]]]`
+        A list of the embeds fields.
     """
 
     type: str = "rich"
@@ -44,14 +64,36 @@ class Embed:
         self.fields: List[Dict[str, Union[str, bool]]] = []
 
     def set_thumbnail(self, url: str) -> None:
-        self.thumbnail = {
-            "url": url,
-        }
+        """
+        Sets the thumnail field.
+
+        Parameters
+        ----------
+        url: :class:`str`
+            The thumbnail url.
+        """
+        self.thumbnail = {"url": url}
 
     def set_video(self, url: str) -> None:
+        """
+        Sets the video field.
+
+        Parameters
+        ----------
+        url: :class:`str`
+            The video url.
+        """
         self.video = {"url": url}
 
     def set_image(self, url: str) -> None:
+        """
+        Sets the image field.
+
+        Parameters
+        ----------
+        url: :class:`str`
+            The image url.
+        """
         self.image = {"url": url}
 
     def set_author(self, name: str, icon_url: str = None) -> None:
@@ -63,19 +105,51 @@ class Embed:
         name: :class:`str`
             The author name.
         icon_url: :class:`str`
-            The icon url to assign.
+            The author icon_url.
         """
         self.author = {"name": name, "icon_url": icon_url}
 
     def set_footer(self, text: str, icon_url: str = None) -> None:
+        """
+        Sets the footer field.
+
+        Parameters
+        ----------
+        text: :class:`str`
+            The footer text.
+        icon_url: :class:`str`
+            The footer icon_url.
+        """
         self.footer = {"text": text, "icon_url": icon_url}
 
     def add_field(
         self, name: str = NO_WIDTH_CHAR, value: str = NO_WIDTH_CHAR, inline: bool = False
     ) -> None:
+        """
+        Adds a field to the embed.
+
+        Parameters
+        ----------
+        name: :class:`str`
+            The field name. Defaults to 
+            an ascii no width char.
+        value: :class:`str`
+            The field value. Defaults to 
+            an ascii no width char.
+        """
         self.fields.append({"name": name, "value": value, "inline": inline})
 
     def _to_dict(self) -> Dict[str, Any]:
+        """
+        Converts the current state to a json-
+        parsiable dictionary.
+
+        Returns
+        -------
+        _dict: :class:`Dict[str, Any]`
+            The json-parsonable dict representing
+            the current embed state.
+        """
         _dict = {
             "title": self.title,
             "description": self.description,
